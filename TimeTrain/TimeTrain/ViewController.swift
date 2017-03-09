e2647c7ed18ee8bfc0b84b1488f08f59f0f41a05
SnowBabies <snowbaby@SnowBaby-2.local>
2017-03-09 22:38:14 +0800

    推送带铃声，没添加时间

--- TimeTrain/TimeTrain/ViewController.swift
+++ TimeTrain/TimeTrain/ViewController.swift
@@ -7,23 +7,88 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
     
     @IBAction func cancel(segue:UIStoryboardSegue){
         
     }
-
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    //推送通知不带铃声
+    @IBAction func sendNotification(_ sender: UIButton) {
+        let center = UNUserNotificationCenter.current()
+        
+        let notificationRequestId = "notificationRequestId"
+        let notificationContent = { () -> UNMutableNotificationContent in
+            
+            let content = UNMutableNotificationContent()
+            content.title = NSString.localizedUserNotificationString(forKey: "你的药到了!", arguments: nil)
+            content.body = NSString.localizedUserNotificationString(forKey: "主人，你的胶囊到了！赶快吃吧！!", arguments: nil)
+            content.sound = UNNotificationSound.default()
+            content.categoryIdentifier = NotificationType.plain.rawValue
+            return content
+        }()
+        
+        let notificationRequest = UNNotificationRequest(identifier: notificationRequestId, content: notificationContent, trigger: UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false))
+        center.add(notificationRequest) { (error) in
+            if let error = error {
+                print("\(error)")
+            }
+        }
+    }
+    //推送通知带铃声
+    @IBAction func sendNotificationWithAttachment(_ sender: UIButton) {
+        let center = UNUserNotificationCenter.current()
+        
+        let notificationRequestId = "notificationRequestId"
+        let notificationAttachmentId = "notificationAttachmentId"
+        
+        let notificationContent = { () -> UNMutableNotificationContent in
+            
+            let url:URL
+            url = MediaType.audioFullLocal.url()
+            
+            let attachment: UNNotificationAttachment?
+            do {
+                
+                attachment = try UNNotificationAttachment(identifier: notificationAttachmentId, url: url, options: nil)
+            } catch {
+                attachment = nil
+            }
+            
+            let content = UNMutableNotificationContent()
+            content.title = NSString.localizedUserNotificationString(forKey: "Here come dat boi!", arguments: nil)
+            content.subtitle = NSString.localizedUserNotificationString(forKey: "Subtitle", arguments: nil)
+            content.body = NSString.localizedUserNotificationString(forKey: "Oh snap!", arguments: nil)
+            content.sound = UNNotificationSound.default()
+            content.categoryIdentifier = NotificationType.serviceExtension.rawValue
+            if let attachment = attachment {
+                content.attachments = [attachment]
+            }
+            return content
+        }()
+        
+        let notificationRequest = UNNotificationRequest(identifier: notificationRequestId, content: notificationContent, trigger: UNTimeIntervalNotificationTrigger(timeInterval: 8, repeats: false))
+        center.add(notificationRequest) { (error) in
+            if let error = error {
+                print("\(error)")
+            }
+        }
+    }
+    
+    
+    
+    
 }
 
